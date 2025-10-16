f6e5bea7325300d102701945b9de57a335d4ed36
Gbolahan Olanrewaju <olanrewajugbolahan226@gmail.com>
2025-10-16 21:02:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateTime() {
+    const timeElement = document.querySelector('[data-testid="test-user-time"]');
+
+    const currentTime = Date.now();
+
+    if (timeElement) {
+        timeElement.textContent = currentTime;
+    }
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
